63f95dc18da4d1ba7cb678b1adc17ebc02e30686
eeknay <eeknay23@googlemail.com>
2012-11-25 16:16:13 +0100

    Update emitter_ws.py

--- emitter_ws.py
+++ emitter_ws.py
@@ -5,7 +5,6 @@ class WSEmitter():
 	def __init__(self, host, port, path):
 		self.ws = websocket.create_connection('ws://' + host + ':' + str(port) + path)
 
-	# Celery task?
 	def send(self, jsonstr):
 		return self.ws.send(jsonstr)
 
